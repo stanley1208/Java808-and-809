103bc63d9a9b5061fe53d976e4666d9a16ccdabf
A8250092 <stanley.mei08@gmail.com>
2023-04-21 17:47:50 +0800

    update

---
+++ 809/src/package12/ex16.java
@@ -0,0 +1,53 @@
+package package12;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+
+public class ex16 {
+
+	public static void main(String[] args) {
+		List<Country>couList=Arrays.asList(
+				new Country("Japan", Country.Continent.ASIA),
+				new Country("Italy", Country.Continent.EUROPE),
+				new Country("Germany", Country.Continent.EUROPE)
+				);
+		Map<Country.Continent, List<String>>regionNames=couList.stream()
+							.collect(Collectors.groupingBy(Country::getRegion,
+															Collectors.mapping(Country::getName, Collectors.toList())));
+		System.out.println(regionNames);
+	}
+
+}
+
+class Country{
+	public enum Continent{
+		ASIA,EUROPE
+	}
+	
+	String name;
+	Continent region;
+	
+	public Country(String na,Continent reg) {
+		name=na;
+		region=reg;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Continent getRegion() {
+		return region;
+	}
+}
+
+
+
+
---
+++ 809/src/package12/ex17.java
@@ -0,0 +1,40 @@
+package package12;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+
+public class ex17 {
+
+	public static void main(String[] args) {
+		List<String>strs=Arrays.asList("Java","Java EE","Java ME","VRSVRSJava");
+		Predicate<String>cf1=s->s.length()>3;
+		Predicate cf2=new CourseFilter() {
+			public boolean test(String s) {
+				return s.contains("Java");
+			}
+		};
+		
+		long c=strs.stream()
+				.filter(cf1)
+				.filter(cf2)
+				.count();
+		System.out.println(c);
+
+	}
+
+}
+
+interface CourseFilter extends Predicate<String>{
+	public default boolean test(String str) {
+		return str.contains("Java");
+	}
+}
+
+
+
